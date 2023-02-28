962e57bfa95d07c39ee9bcff6e53a31c0d7ccdca
Aniket-Nagwekar-0208 <asnagwekar0109@gmail.com>
2023-02-28 07:27:01 +0530

    First

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import Home from "../components/MainPage/Home"
+import FlashDeals from "../components/flashDeals/FlashDeals"
+import TopCate from "../components/top/TopCate"
+import NewArrivals from "../components/newarrivals/NewArrivals"
+import Discount from "../components/discount/Discount"
+import Shop from "../components/shops/Shop"
+import Announcement from "../components/announcement/Announcement"
+import Wrapper from "../components/wrapper/Wrapper"
+
+const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
+  return (
+    <>
+      <Home CartItem={CartItem} />
+      <FlashDeals productItems={productItems} addToCart={addToCart} />
+      <TopCate />
+      <NewArrivals />
+      <Discount />
+      <Shop shopItems={shopItems} addToCart={addToCart} />
+      <Announcement />
+      <Wrapper />
+    </>
+  )
+}
+
+export default Pages
